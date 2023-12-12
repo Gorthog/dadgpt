bf760d8fff7c5c40a41cedc6de9ff49e5963db5d
Amir Shiri <15230040+Gorthog@users.noreply.github.com>
2023-12-12 18:53:10 +0200

    test changes for deployment

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,7 +5,7 @@ import { Query } from "~/components/Query";
 export default function Home() {
   return (
     <main>
-      <Title>Hello DadGPT</Title>
+      <Title>Hello DadGPT v0.0.1</Title>
       <h1>DadGPT</h1>
       <Query />
       <Append />
