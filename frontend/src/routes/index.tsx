0eecfb8ed9e22848a49e5e60b994c1552365a259
Amir Shiri <15230040+Gorthog@users.noreply.github.com>
2023-12-12 18:56:47 +0200

    update title

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,8 +5,8 @@ import { Query } from "~/components/Query";
 export default function Home() {
   return (
     <main>
-      <Title>Hello DadGPT v0.0.1</Title>
-      <h1>DadGPT</h1>
+      <Title>DadGPT</Title>
+      <h1>DadGPT v0.0.1</h1>
       <Query />
       <Append />
     </main>
