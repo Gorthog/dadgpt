e33d69f7148dc78aca80dfdf3004d44ee2db061d
Amir Shiri <15230040+Gorthog@users.noreply.github.com>
2023-12-31 23:57:09 +0200

    update query css

--- frontend/src/components/Query.tsx
+++ frontend/src/components/Query.tsx
@@ -17,9 +17,11 @@ export function Query() {
 
   return (
     <form onSubmit={handleClick}>
-      <input ref={refQuery} type="text" />
-      <button type="submit">Search</button>
-      <div>{response.loading ? "Loading..." : response()}</div>
+      <div style="display: flex; justify-content: center; padding: 20px; width: 100%; flex-direction: column; align-items: center;">
+        <input ref={refQuery} type="text" style="width: 600px; height: 35px; padding: 3px 10px; font-size: 16px; border-radius: 20px; margin-bottom: 10px;" />
+        <button type="submit" style="width: 120px; height: 36px; font-size: 14px;">Search</button>
+        <div>{response.loading ? "Loading..." : response()}</div>
+      </div>
     </form>
   );
 }
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <Title>DadGPT</Title>
-      <h1>DadGPT v0.0.1</h1>
+      <h1>DadGPT</h1>
       <Query />
       <Append />
     </main>
