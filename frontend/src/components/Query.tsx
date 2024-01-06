d1f5568a1eb92617daed646c0d2dc182d1868abd
Amir Shiri <15230040+Gorthog@users.noreply.github.com>
2024-01-06 20:53:06 +0200

    Add disabled state to search button

--- frontend/src/components/Query.tsx
+++ frontend/src/components/Query.tsx
@@ -7,14 +7,20 @@ export function Query() {
   let refQuery: HTMLDivElement | undefined;
 
   const [query, setQuery] = createSignal<string>();
+  const [disabled, setDisabled] = createSignal<boolean>(true);
 
   const [response] = createResource(query, async (q) =>
     (await (await fetch(`https://dadgpt-dbfrvs4mzq-zf.a.run.app?query=${q}`)).json()).data
   );
 
+  const getTextareaValue = (textarea: HTMLDivElement | undefined) => {
+    // ref and inputRef to TextField didn't work. this is a workaround
+    return (textarea?.querySelector('#search-textarea') as HTMLTextAreaElement).value || "";
+  }
+
   const handleClick = (e: Event) => {
     e.preventDefault();
-    setQuery((refQuery?.querySelector('#search-textarea') as HTMLTextAreaElement).value || "");
+    setQuery(getTextareaValue(refQuery));
   };
 
   return (
@@ -25,8 +31,9 @@ export function Query() {
           ref={refQuery}
           multiline
           maxRows={5}
+          onChange={(e) => { setDisabled(!e.target.value) }}
         />
-        <Button class={styles.searchButton} variant="contained" type="submit">Search</Button>
+        <Button class={styles.searchButton} variant="contained" type="submit" disabled={disabled()}>Search</Button>
         <div class={styles.searchResult} >{response.loading ? "Loading..." : response()}</div>
       </div>
     </form>
